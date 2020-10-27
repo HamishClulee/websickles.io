61e47da56fa04729caac64c10a3a1a2f6957d019
HamishClulee <hamish.clulee@hotmail.com>
2020-10-27 12:33:57 +0700

    Further improvemnets to typing for vuex

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,13 +1,32 @@
-import { createStore } from 'vuex'
-
+import {
+    createStore,
+    Store as VuexStore,
+    CommitOptions,
+    DispatchOptions,
+  } from 'vuex'
+import { State } from '../../interfaces/IState'
 import { state } from './state'
-import { mutations } from './mutations'
-// import getters from './getters'
-// import actions from './actions'
+import { mutations, Mutations } from './mutations'
+import { getters, Getters } from './getters'
 
 export default createStore({
     state,
     mutations,
-    // actions,
-    // getters,
+    getters,
 })
+
+export type Store = Omit<VuexStore<State>, 'getters' | 'commit' | 'dispatch'> & {
+
+    commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
+        key: K,
+        payload: P,
+        options?: CommitOptions
+    ): ReturnType<Mutations[K]>
+
+    } & {
+
+    getters: {
+        [K in keyof Getters]: ReturnType<Getters[K]>
+    }
+    
+}
